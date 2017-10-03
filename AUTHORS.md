8331ca90d2efba4f688f70a18b7443b81a4bdce0
Jenn Wilde <jennowilde@gmail.com>
2017-10-03 15:36:47 -0700

    initial commit

---
+++ AUTHORS.md
@@ -0,0 +1,4 @@
+keygen was written in 2017 on Muwekma Ohlone ancestral lands in Oakland, CA
+
+The following is a list of contributors:
+Jenn Wilde <jennowilde@gmail.com>
---
+++ spec/keygen-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import KeygenView from '../lib/keygen-view';
+
+describe('KeygenView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
